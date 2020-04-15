e8efdb8d2c8bb832b6b530242392127b26067cac
0212kr <0212kr@gmail.com>
2020-04-15 21:06:20 +0900

    Humm.. sorry instructor

--- a003_listener/src/main/java/com/lec/android/a003_widget/Main2Activity.java
+++ a003_listener/src/main/java/com/lec/android/a003_widget/Main2Activity.java
@@ -3,6 +3,7 @@ package com.lec.android.a003_widget;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class Main2Activity extends AppCompatActivity {
 
@@ -11,5 +12,17 @@ public class Main2Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
-    }
+
+        Button button7 = findViewById(R.id.button1);
+        Button button7 = findViewById(R.id.button2);
+        Button button7 = findViewById(R.id.button3);
+        Button button7 = findViewById(R.id.button4);
+        Button button7 = findViewById(R.id.button7);
+        Button button7 = findViewById(R.id.button7);
+        Button button7 = findViewById(R.id.button7);
+        Button button7 = findViewById(R.id.button7);
+        Button button7 = findViewById(R.id.button7);
+
+
+    }//onCreate end
 }
